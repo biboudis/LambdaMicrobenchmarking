40139026826a5b7ceb0a755bd49f3eebe8e5e5e4
Aggelos Biboudis <biboudis@gmail.com>
2014-05-26 00:23:12 +0300

    Factored out a small utility from the Clash of the Lambdas.

---
+++ LambdaMicrobenchmarking/Compiler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaMicrobenchmarking
+{
+    class Compiler
+    {
+        static public T ConsumeValue<T>(T dummy)
+        {
+            return dummy;
+        }
+
+    }
+}
---
+++ LambdaMicrobenchmarking/Script.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaMicrobenchmarking
+{
+    public class Script<T>
+    {
+        static public int Iterations { get; set; }
+        static public int WarmupIterations { get; set; }
+        private Tuple<String, Func<T>>[] actions { get; set; }
+
+        private Script(params Tuple<String, Func<T>>[] actions)
+        {
+            this.actions = actions;
+        }
+
+        public static Script<T> Of(params Tuple<String, Func<T>>[] actions)
+        {
+            return new Script<T>(actions);
+        }
+
+        public Script<T> RunAll()
+        {
+            Console.WriteLine("{0,-25} \t{1,10} {2,6:0.00} {3,4:0.00} {4}", "Benchmark", "Mean", "Mean Error", "Sdev", "Units");
+
+            actions.Select(action => new Run<T>(action)).ToList().ForEach(run => run.Measure());
+            return this;
+        }
+    }
+}
