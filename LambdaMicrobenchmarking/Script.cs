8c6080307ab9f3d996903715965243bded432244
Aggelos Biboudis <biboudis@users.noreply.github.com>
2017-02-17 11:34:05 +0200

    Revert "Allow users to actually modify iteration count"

--- LambdaMicrobenchmarking/Script.cs
+++ LambdaMicrobenchmarking/Script.cs
@@ -20,17 +20,8 @@ namespace LambdaMicrobenchmarking
     }
     public class Script<T>
     {
-        static public int Iterations
-        {
-            get { return Run<T>.iterations; }
-            set { Run<T>.iterations = value; }
-        }
-
-        static public int WarmupIterations
-        {
-            get { return Run<T>.warmups; }
-            set { Run<T>.warmups = value; }
-        }
+        static public int Iterations { get; set; }
+        static public int WarmupIterations { get; set; }
 
         private List<Tuple<String, Func<T>>> actions { get; set; }
 
